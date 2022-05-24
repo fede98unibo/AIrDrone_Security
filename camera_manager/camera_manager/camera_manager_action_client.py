25ee83ea1c35a8cf4207f0cbca7269dc90365f16
Simone Giordani <63203939+Zumo09@users.noreply.github.com>
2022-05-24 23:01:33 +0200

    video oks

--- camera_manager/camera_manager/camera_manager_action_client.py
+++ camera_manager/camera_manager/camera_manager_action_client.py
@@ -1,3 +1,95 @@
+# # Copyright 2019 Open Source Robotics Foundation, Inc.
+# #
+# # Licensed under the Apache License, Version 2.0 (the "License");
+# # you may not use this file except in compliance with the License.
+# # You may obtain a copy of the License at
+# #
+# #     http://www.apache.org/licenses/LICENSE-2.0
+# #
+# # Unless required by applicable law or agreed to in writing, software
+# # distributed under the License is distributed on an "AS IS" BASIS,
+# # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# # See the License for the specific language governing permissions and
+# # limitations under the License.
+
+# # from example_interfaces.action import Fibonacci
+# from airdrone_actions.action import TakeVideo
+
+# import rclpy
+# from rclpy.action import ActionClient
+# from rclpy.node import Node
+
+
+# class MinimalActionClient(Node):
+
+#     def __init__(self):
+#         super().__init__('minimal_action_client')
+#         self._action_client = ActionClient(self, TakeVideo, 'take_video')
+
+#     def cancel_done(self, future):
+#         cancel_response = future.result()
+#         if len(cancel_response.goals_canceling) > 0:
+#             self.get_logger().info('Goal successfully canceled')
+#         else:
+#             self.get_logger().info('Goal failed to cancel')
+
+#         rclpy.shutdown()
+
+#     def goal_response_callback(self, future):
+#         goal_handle = future.result()
+#         if not goal_handle.accepted:
+#             self.get_logger().info('Goal rejected :(')
+#             return
+
+#         self._goal_handle = goal_handle
+
+#         self.get_logger().info('Goal accepted :)')
+
+#         # Start a 2 second timer
+#         self._timer = self.create_timer(2.0, self.timer_callback)
+
+#     def feedback_callback(self, feedback):
+#         self.get_logger().info(f'Received feedback: {type(feedback).__name__}')
+
+#     def timer_callback(self):
+#         self.get_logger().info('Canceling goal')
+#         # Cancel the goal
+#         future = self._goal_handle.cancel_goal_async()
+#         future.add_done_callback(self.cancel_done)
+
+#         # Cancel the timer
+#         self._timer.cancel()
+
+#     def send_goal(self):
+#         self.get_logger().info('Waiting for action server...')
+#         self._action_client.wait_for_server()
+
+#         goal_msg = TakeVideo.Goal()
+#         goal_msg.video_name = "cacca"
+
+#         self.get_logger().info('Sending goal request...')
+
+#         self._send_goal_future = self._action_client.send_goal_async(
+#             goal_msg,
+#             feedback_callback=self.feedback_callback)
+
+#         self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+
+# def main(args=None):
+#     rclpy.init(args=args)
+
+#     action_client = MinimalActionClient()
+
+#     action_client.send_goal()
+
+#     rclpy.spin(action_client)
+
+
+# if __name__ == '__main__':
+#     main()
+
+
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
@@ -13,21 +105,39 @@ class CameraActionClient(Node):
         self._action_client = ActionClient(self, TakeVideo, "take_video")
         self.elapsed_time = 0
         self.bridge = CvBridge()
+        self.f = 0
 
     def send_goal(self, video_name):
+        self.get_logger().info("Waiting for action server...")
+        self._action_client.wait_for_server()
+
         goal_msg = TakeVideo.Goal()
         goal_msg.video_name = video_name
 
         self._action_client.wait_for_server(10)
 
-
         self._send_goal_future = self._action_client.send_goal_async(
             goal_msg, feedback_callback=self.feedback_callback
         )
         self._send_goal_future.add_done_callback(self.goal_response_callback)
-    
+
     def cancel_goal(self):
-        self._send_goal_future.cancel()
+        self.get_logger().info("Canceling goal")
+        # Cancel the goal
+        future = self._goal_handle.cancel_goal_async()
+        future.add_done_callback(self.cancel_done)
+
+        # Cancel the timer
+        self._timer.cancel()
+
+    def cancel_done(self, future):
+        cancel_response = future.result()
+        if len(cancel_response.goals_canceling) > 0:
+            self.get_logger().info("Goal successfully canceled")
+        else:
+            self.get_logger().info("Goal failed to cancel")
+
+        rclpy.shutdown()
 
     def goal_response_callback(self, future):
         goal_handle = future.result()
@@ -36,9 +146,13 @@ class CameraActionClient(Node):
             return
 
         self.get_logger().info("Goal accepted :)")
+        self._goal_handle = goal_handle
 
-        self._get_result_future = goal_handle.get_result_async()
-        self._get_result_future.add_done_callback(self.get_result_callback)
+        # self._get_result_future = goal_handle.get_result_async()
+        # self._get_result_future.add_done_callback(self.get_result_callback)
+
+        # Start a 2 second timer
+        self._timer = self.create_timer(2.0, self.cancel_goal)
 
     def get_result_callback(self, future):
         result = future.result().result
@@ -48,7 +162,12 @@ class CameraActionClient(Node):
     def feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
         self.elapsed_time = feedback.elapsed_time
-        self.get_logger().info(f"Received feedback: {feedback.elapsed_time}")
+
+        if self.f % 30 == 0:
+            self.f = 0 
+            self.get_logger().info(f"Received feedback: {self.elapsed_time}")
+        self.f += 1 
+        
         image = self.bridge.compressed_imgmsg_to_cv2(feedback.image)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         cv2.imshow("video", image)
@@ -62,10 +181,13 @@ def main(args=None):
 
     action_client.send_goal("test")
 
-    while action_client.elapsed_time < 3:
-        rclpy.spin_once(action_client)
+    # while action_client.elapsed_time < 3:
+    #     rclpy.spin_once(action_client)
 
-    action_client.cancel_goal()
+    # action_client.cancel_goal()
+
+    rclpy.spin(action_client)
+
 
 if __name__ == "__main__":
     main()
--- camera_manager/camera_manager/camera_manager_server.py
+++ camera_manager/camera_manager/camera_manager_server.py
@@ -1,5 +1,4 @@
 import time
-import threading
 
 from airdrone_actions.srv import TakePicture
 from airdrone_actions.action import TakeVideo
@@ -7,30 +6,31 @@ from sensor_msgs.msg import Image
 
 import rclpy
 from rclpy.node import Node
-from rclpy.action import ActionServer, CancelResponse
 from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.action.server import ActionServer, CancelResponse, ServerGoalHandle
 
 from cv_bridge import CvBridge
 import cv2
 
-SAVE_PATH = "/home/simone/Videos"
+import numpy as np
+
+SAVE_PATH = '/home/simone/Videos'
 
 
 class ImageSubNode(Node):
     def __init__(self):
-        super().__init__("img_sub_node")
+        super().__init__('img_sub_node')
         self.image_subscriber = self.create_subscription(
-            Image, "/camera/image_raw", self.save_callback, 1
+            Image, '/camera/image_raw', self.save_callback, 1
         )
         self._image = None
-        self.get_logger().info("Ready to grab frames")
+        self.get_logger().info('Ready to grab frames')
 
     def save_callback(self, img: Image) -> None:
         self._image = img
 
     def get_frame(self) -> Image:
-        self.get_logger().info("Getting Frame")
-
         self._image = None
 
         while self._not_ready():
@@ -39,99 +39,110 @@ class ImageSubNode(Node):
         return self._image
 
     def _not_ready(self) -> bool:
-        return self._image is None
+        return self._image is None  # and geo is None ....
 
 
 class CameraService(Node):
     def __init__(self):
-        super().__init__("camera_service")
+        super().__init__('camera_service')
         self._srv_take = self.create_service(
-            TakePicture, "take_picture", self.take_picture_service
+            TakePicture, 'take_picture', self.take_picture_service
         )
 
         self.bridge = CvBridge()
         self.img_node = ImageSubNode()
-        # self.video_node = VideoSubNode()
 
         self._action_server = ActionServer(
             self,
             TakeVideo,
-            "take_video",
-            self.take_video_action_service,
+            'take_video',
+            execute_callback=self.take_video_action_service,
+            callback_group=ReentrantCallbackGroup(),
             cancel_callback=self.cancel_callback,
         )
 
-        self._run = False
         self.video_writer = None
         self.fps = 30
-        self.get_logger().info("Ready to serve")
+        self.get_logger().info('Ready to serve')
 
     def take_picture_service(self, request, response):
         self.get_logger().info(
-            f"""Incoming request:
+            f'''Incoming request:
                 detections: {request.include_detections} 
                 geo: {request.include_geolocalization}
-            """
+            '''
         )
         response.image = self.img_node.get_frame()
 
         return response
 
-    def take_video_action_service(self, goal_handle):
-        self.get_logger().info("Taking video...")
+    async def take_video_action_service(self, goal_handle: ServerGoalHandle):
+        self.get_logger().info('Taking video...')
         result = TakeVideo.Result()
-        goal_handle.succeed()
 
-        fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
-        self.video_writer = None
+        video_path = f'{SAVE_PATH}/{goal_handle.request.video_name}.avi'
+        result.video_path = video_path
+
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
 
         feedback_msg = TakeVideo.Feedback()
         start = time.time()
 
-        video_path = f"{SAVE_PATH}/{goal_handle.request.video_name}.mp4"
-        result.video_path = video_path
-
-        self._run = True
-
-        rate = self.create_rate(self.fps)
+        f = 0
 
         while rclpy.ok():
-            if not self._run:
+
+            if goal_handle.is_cancel_requested:
                 break
+
             feedback_msg.elapsed_time = int(time.time() - start)
+            if f % 30 == 0:
+                self.get_logger().info('Getting Frames')
+                f = 0
+            f += 1
+            
             frame = self.img_node.get_frame()
             image = self.bridge.imgmsg_to_cv2(frame)
 
+            self.get_logger().warn(f"{type(image).__name__}")
+
             if self.video_writer is None:
+                self.get_logger().info('Init Writer')
                 image_w, image_h, _ = image.shape
                 self.video_writer = cv2.VideoWriter(
                     video_path,
                     fourcc,
-                    self.fps,
-                    (image_w, image_h),
+                    self.fps, 
+                    (640, 480) # TODO: controllare perchè fa il cazzone con queste shapes
+                    # (int(image_w), int(image_h)),
                 )
+
             feedback_msg.image = self.bridge.cv2_to_compressed_imgmsg(image)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             self.video_writer.write(image)
             goal_handle.publish_feedback(feedback_msg)
-            rate.sleep()
-        
-        # già rilasciato nella cancel callback (?)
+
         if self.video_writer is not None:
             self.video_writer.release()
-            
+            self.get_logger().info('Releasing video')
+            self.video_writer = None
+
+        
+        goal_handle.canceled()
+        self.get_logger().info('Done')
+
         return result
 
     def cancel_callback(self, cancel_request):
-        self.get_logger().warn(f"{cancel_request}")
-        self._run = False
-        if self.video_writer is not None:
-            self.video_writer.release()
-            return CancelResponse.ACCEPT
-        return CancelResponse.REJECT
+        self.get_logger().warn(f'Received cancel request')
+        return CancelResponse.ACCEPT
+
 
     def destroy_node(self) -> bool:
         if self.video_writer is not None:
             self.video_writer.release()
+        self._action_server.destroy()
+        self.img_node.destroy_node()
         return super().destroy_node()
 
 
@@ -139,15 +150,16 @@ def main(args=None):
     rclpy.init(args=args)
 
     minimal_service = CameraService()
+    executor = MultiThreadedExecutor()
 
     try:
-        rclpy.spin(minimal_service, MultiThreadedExecutor(2))
+        rclpy.spin(minimal_service, executor=executor)
     except KeyboardInterrupt:
-        minimal_service.get_logger().info("Closing Node (Keyboard Interrupt)")
+        minimal_service.get_logger().info('Closing Node (Keyboard Interrupt)')
     finally:
         minimal_service.destroy_node()
         rclpy.shutdown()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
